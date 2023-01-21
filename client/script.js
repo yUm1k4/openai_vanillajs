d1897c5221777367fe9011fcd26b3bbfd02ca669
Zainudin Abdullah <zaiabdullah99@gmail.com>
2023-01-21 14:47:37 +0700

    (feat) penambahan hit ke server dan perbaikan typo serta penempatan file .env

--- client/script.js
+++ client/script.js
@@ -25,7 +25,7 @@ function typeText(element, text) {
 
     let interval = setInterval(() => {
         if(index < text.length) {
-            element.innerHTML += text.chartAt(index); // menambahkan text per huruf
+            element.innerHTML += text.charAt(index); // menambahkan text per huruf
             index++;
         } else {
             clearInterval(interval);
@@ -77,6 +77,35 @@ const handleSubmit = async (e) => {
     const messageDiv = document.getElementById(uniqueId);
 
     loader(messageDiv);
+
+    // mengirim data ke server -> bot's response
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt'),
+        })
+    });
+
+    clearInterval(loadInterval);
+
+    messageDiv.innerHTML = '';
+
+    if (response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+
+        console.log(parsedData);
+        typeText(messageDiv, parsedData);
+    } else {
+        const err = await response.text();
+
+        messageDiv.innerHTML = "Something went wrong!";
+
+        alert(err);
+    }
 }
 
 form.addEventListener('submit', handleSubmit);
